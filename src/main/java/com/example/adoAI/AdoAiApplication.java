fbf4a4411250ad98e3030fbad8596866c1a6eebf
gleisson p dias <79737198+GleissonPDias@users.noreply.github.com>
2026-08-29 22:20:42 -0300

    Commit inicial
    Commit Inicial com os arquivos spring initializr

---
+++ src/main/java/com/example/adoAI/AdoAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.adoAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adoAI/AdoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adoAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
